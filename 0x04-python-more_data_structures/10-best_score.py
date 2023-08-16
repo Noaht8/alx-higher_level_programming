8dd2c2da9d021db7ba884b71b8d9efe4faf10e02
Noah Tsegay <noahtsegay8@gmail.com>
2023-08-16 11:06:07 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,8 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    sorted_list = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
-    conv_list_to_dict = dict(sorted_list)
-    biggest_value = list(conv_list_to_dict.values())[0]
-    return biggest_value
+    else:
+        sorted_list = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+        conv_list_to_dict = dict(sorted_list)
+        biggest_value = list(conv_list_to_dict.values())[0]
+        return biggest_value
