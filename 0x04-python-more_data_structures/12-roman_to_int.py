63e9c944acde763399243532cfff793b9ca65ea2
Noah Tsegay <noahtsegay8@gmail.com>
2023-08-16 11:16:41 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_n = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]]
+
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    result = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_values[char]
+
+        if value >= prev_value:
+            result += value
         else:
-            roman_n += roman_d[roman_string[j]]
-    return roman_n
+            result -= value
+
+        prev_value = value
+
+    return result
