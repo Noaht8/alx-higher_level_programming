f50e07ec9e1915959edbca83d68ee64908dfd172
Noah Tsegay <noahtsegay8@gmail.com>
2023-08-16 11:07:37 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,4 +7,4 @@ def best_score(a_dictionary):
         sorted_list = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
         conv_list_to_dict = dict(sorted_list)
         biggest_value = list(conv_list_to_dict.values())[0]
-        return biggest_value
+    return biggest_value
